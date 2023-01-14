97edfb61bb6559aeb6fe62d7a3a5fea2e2c1ab0e
Krzysztof W <45880989+iProXDPL@users.noreply.github.com>
2023-01-14 23:44:40 +0100

    Update and rename All.h to MyAll.h

--- All.h
+++
@@ -1,5 +0,0 @@
-#include <Buttons.h>
-#include <Diagrams.h>
-#include <Math.h>
-#include <StepMotors.h>
-#include <Other.h>
---
+++ MyAll.h
@@ -0,0 +1,5 @@
+#include <MyButtons.h>
+#include <MyDiagrams.h>
+#include <MyMath.h>
+#include <MyStepMotors.h>
+#include <MyOther.h>
